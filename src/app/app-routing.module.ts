58f81d7f8ab3aed0ae22e6608f4c3287b5e1e2c8
Jeffree98 <familiamorales20@gmail.com>
2019-11-09 18:11:36 -0600

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: './componentes/login/login.module#LoginPageModule' },
+  { path: 'inicio', loadChildren: './componentes/inicio/inicio.module#InicioPageModule' },
+];
+
+@NgModule({ 
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBDtxigKxamgcT7yTjcPos3srvywpjhzPY",
+  authDomain: "logito-683d1.firebaseapp.com",
+  databaseURL: "https://logito-683d1.firebaseio.com",
+  projectId: "logito-683d1",
+  storageBucket: "logito-683d1.appspot.com",
+  messagingSenderId: "750692758839",
+  appId: "1:750692758839:web:f2c00a9667bf34174c96b2",
+  measurementId: "G-R1SET7E5G8"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
